a4832b47a02f6750d58bfb00de355236b30af580
muhammad-fahad-hussain <mfahadpk176@gmail.com>
2024-03-12 20:23:44 +0500

    Dart

---
+++ Assignments/Assignment in Dart/DartProgram.dart
@@ -0,0 +1,34 @@
+// 1. Write a program to print your name in Dart.
+
+// void main() {
+//   print("Muhammad Fahad");
+// }
+
+// ========================================================================================
+
+/* Write a program to print Hello I am “John Doe” and Hello I’am “John 
+Doe” with single and double quotes.*/
+// void main() {
+//   print('Hello I am "John Doe"');
+//   print("Hello, I'm 'John Doe'");
+// }
+
+// ========================================================================================
+
+// 3. Declare constant type of int set value 7.
+
+// void main() {
+//   const int constent = 7;
+//   print("Const Number is $constent");
+// }
+
+// =============================================================================================
+// 4. Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100
+void main() {
+  int p = 4;
+  int t = 2;
+  int r = 6;
+  double interest;
+  interest = (p * t * r) / 100;
+  print("Interest= $interest");
+}
