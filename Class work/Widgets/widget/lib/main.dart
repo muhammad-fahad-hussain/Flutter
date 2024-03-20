54efd9f53ea36ff61d4e426dde03d6eb7040a739
Muhammad Fahad <mfahadpk176@gmail.com>
2024-03-20 23:55:46 +0500

    Update main.dart

--- Class work/Widgets/widget/lib/main.dart
+++ Class work/Widgets/widget/lib/main.dart
@@ -6,4 +6,34 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Week Lab 3"),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blue, Colors.pink],
+                begin: Alignment.bottomCenter,
+              ),
+            ),
+            height: 400,
+            width: 400,
+            child: Center(
+              child: Text(
+                "Muhammad Fahad",
+                style: TextStyle(color: Colors.cyan),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
