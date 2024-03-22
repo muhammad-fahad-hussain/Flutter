ece9010dd24ea21fcaee5a0be5e7e8b15eb8d659
Muhammad Fahad <mfahadpk176@gmail.com>
2024-03-22 10:31:06 +0500

    lab4

--- Lab Tasks/lab4/lib/main.dart
+++ Lab Tasks/lab4/lib/main.dart
@@ -18,51 +18,88 @@ class MyApp extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Container(
                   width: 100,
                   height: 100,
-                  color: Colors.blue,
-                  margin: EdgeInsets.all(10),
-                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.horizontal(
+                      left: Radius.circular(10),
+                      right: Radius.circular(0),
+                    ),
+                    gradient: LinearGradient(
+                      colors: [Colors.lime, Colors.cyan],
+                    ),
+                  ),
                   child: Center(
-                    child: Text("Container 1"),
+                    child: Text(
+                      "Container 1",
+                      style: TextStyle(fontSize: 15),
+                    ),
                   ),
                 ),
-                Icon(Icons.star, size: 50),
+                Icon(Icons.stairs_sharp, size: 50),
               ],
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 50),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  color: Colors.orange,
+                  width: 200,
                   margin: EdgeInsets.all(10),
-                  padding: EdgeInsets.all(10),
-                  child: Image.asset(
-                    'assets/download.jpeg',
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          width: 6,
+                          color: const Color.fromARGB(221, 224, 105, 105)),
+                      borderRadius: BorderRadius.circular(16)),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset('assets/download.jpeg'),
                   ),
                 ),
               ],
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 50),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Container(
                   width: 100,
                   height: 100,
-                  color: Colors.red,
-                  margin: EdgeInsets.all(10),
-                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(
+                      colors: [Colors.white54, Colors.lightBlueAccent],
+                    ),
+                    border: Border.all(
+                      color: Colors.indigo,
+                      width: 2,
+                    ),
+                  ),
                   child: Center(
                     child: Text("Container 2"),
                   ),
                 ),
-                Icon(Icons.music_note, size: 50),
-                Text("Hello", style: TextStyle(fontSize: 24)),
+                Icon(
+                  Icons.access_time_rounded,
+                  size: 50,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print("pressed the button....");
+                  },
+                  child: Text(
+                    "Fahad",
+                    style: TextStyle(
+                      color: Colors.deepOrange,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
               ],
             ),
           ],
